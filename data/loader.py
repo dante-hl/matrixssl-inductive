ceb5c9c3b3b3a7850321640a11c6ea8fad855d05
Dante Lokitiyakul <admin@hnt-06957.apn.wlan.private.upenn.edu>
2024-02-22 18:12:07 -0500

    changed generate_cube_data to split data into train and validation sets

--- data/loader.py
+++ data/loader.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def generate_cube_data(n: int = 50000, v: int = 12500, d: int = 50, k: int = 10, labelling: str = 'single'):
     """
-    Generates `n` points in the `d`-dimensional hypercube {-1, 1}^d, with a train-test split of n-v:v.
+    Generates `n` points in the `d`-dimensional hypercube {-1, 1}^d, with a train-val split of n-v:v.
 
     Training points are sampled uniformly in the d-dimensional hypercube {-1, 1}^d. Two augmentations are produced from
     each point by independently scaling each of the last d-k dimensions by a uniformly sampled constant within (0, 1].
@@ -15,7 +15,7 @@ def generate_cube_data(n: int = 50000, v: int = 12500, d: int = 50, k: int = 10,
     dimension chosen randomly from the first k invariant dimensions. If `labelling` is 'all', then the label is the
     sign of some random linear classifier on the invariant dimensions, with weights sampled from a Standard Gaussian.
 
-    Returns train-test data tuple (x1, x2, y), (val_x, val_y)
+    Returns train-val data tuple (x1, x2, y), (val_x, val_y)
     Training set: x1, x2, (n-v, d) arrays of augmented data, and labels y (n-v,).
     Validation set: val_x (v, d), val_y (v,)
     """
