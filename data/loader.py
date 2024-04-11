2ce558c8b5103137e373124b3cf86630a63b1cc4
Dante Lokitiyakul <admin@brb-11151.apn.wlan.private.upenn.edu>
2024-04-11 01:28:27 -0400

    implement 'add' augmentations to generate_cube_data

--- data/loader.py
+++ data/loader.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # %%
 # ignoring labelling: str = 'single', for now, add more if you need
-def generate_cube_data(n: int = (2 ** 16) + 12500, v: int = 12500, d: int = 50, k: int = 10, weights=None):
+def generate_cube_data(n: int = (2 ** 16) + 12500, v: int = 12500, d: int = 50, k: int = 10, augmentation='mult'):
     """
     Generates `n` points in the `d`-dimensional hypercube {-1, 1}^d, with a train-val split of n-v:v.
 
@@ -14,34 +14,61 @@ def generate_cube_data(n: int = (2 ** 16) + 12500, v: int = 12500, d: int = 50, 
     v: number of validation points
     d: total dimension of each datum
     k: number of invariant features
-    weights: (k,) array of weights for generating data, sampled from N(0, 1) if None
 
-    Returns train-val data tuple (x1, x2, y), (val_x, val_y)
+    augmentation: string specifying type of augmentation: note that this also specifies the labeling function, since augmentations must be label preserving.
+        'mult': scales each invariant dimension independently by a scalar tau ~ U(0, 1]. resulting labeling function  uses a linear classifier with randomly sampled weights from N(0, 1) on the first k (invariant) dimensions of data
+        'add': for each pair of dimensions, samples tau ~ U(0, 1], then adds to one dimension and subtracts from the other. requires an even number of spurious dimensions to guarantee label preservation. labeling function returns the sign of the sum of all spurious dimension values.
+
+    Returns dictionary: {"train":(x1, x2, y), "val":(val_x, val_y), "truth":truth}
     Training set: x1, x2, (n-v, d) arrays of augmented data, and labels y (n-v,).
     Validation set: val_x (v, d), val_y (v,)
+    Truth: if augmentation='mult', then the true linear classif weights. if 'add', then the string 'spurious_sum' (to indicate summing spurious dimensions)
     """
     assert k < d  # can it be equal..?
     natural = 2 * torch.rand((n, d)) - 1  # uniform in (-1, 1)
     val_x = natural[-v:, :]  # (v, d)
     train_x = natural[:n - v, :]  # (n-v, d)
-    
-    tau1, tau2 = torch.ones_like(train_x), torch.ones_like(train_x)
-    tau1[:, k:] *= torch.rand((n - v, d - k))
-    tau2[:, k:] *= torch.rand((n - v, d - k))
-    x1, x2 = train_x * tau1, train_x * tau2
+    # print(f'Train x {train_x}')
 
-    true_w = torch.zeros(d)
-    if weights:
-        assert weights.shape[0] == k
-        true_w[:k] = weights
-    else: 
-        true_w[:k] = torch.randn(k)
+    if augmentation == 'mult':
+        # augment by scaling down invariant dimensions
+        tau1, tau2 = torch.ones_like(train_x), torch.ones_like(train_x)
+        tau1[:, k:] *= torch.rand((n - v, d - k))
+        tau2[:, k:] *= torch.rand((n - v, d - k))
+        x1, x2 = train_x * tau1, train_x * tau2
+        # instantiate labeling function weights (truth)
+        truth = torch.zeros(d)
+        truth[:k] = torch.randn(k)
+        # create labels, change from {-1, 1} to {0, 1} labels - for linear classification loss
+        y = (torch.sign(train_x @ truth) + 1)/2
+        val_y = (torch.sign(val_x @ truth) + 1)/2
+    elif augmentation == 'add':
+        # augment by adding and subtracting tau
+        assert (d - k) % 2 == 0
+        half = int((d-k)/2) # half number of spurious features
+        tau1, tau2 = torch.zeros_like(train_x), torch.zeros_like(train_x)
+
+        additive1 = torch.rand((n-v, half))
+        tau1[:, k:k + half] += additive1
+        tau1[:, k+half:] -= additive1
+
+        additive2 = torch.rand((n-v, half))
+        tau2[:, k:k + half] += additive2
+        tau2[:, k+half:] -= additive2
+
+        # print(f'Additive1: {additive1}')
+        # print(f'Additive2: {additive2}')
+
+        x1, x2 = train_x + tau1, train_x + tau2
+        # create labels: {-1, 1} -> {0, 1}
+        y = (torch.sign(torch.sum(train_x[:, k:], dim=1)) + 1)/2
+        val_y = (torch.sign(torch.sum(val_x[:, k:], dim=1)) + 1)/2
+
+        truth = 'spurious_sum'
+    else:
+        raise Exception("Invalid augmentation, expected one of {'mult', 'add'}")
     # print(f"weights: {true_w}")
 
-    # change from {-1, 1} to {0, 1} labels - for linear classification loss
-    y = (torch.sign(train_x @ true_w) + 1)/2
-    val_y = (torch.sign(val_x @ true_w) + 1)/2
-
-    return {"train":(x1, x2, y), "val":(val_x, val_y), "true_weights":true_w}
+    return {"train":(x1, x2, y), "val":(val_x, val_y), "truth":truth}
 
 # %%
